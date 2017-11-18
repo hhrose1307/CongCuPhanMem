4401c0f5735a5c901897c57de1ed1f71468b17b6
nguyenvantu <nguyenvantu439@gmail.com>
2017-11-19 00:11:14 +0700

    @CCPM-12 #COMMENT TAOLOAISACHCONTROLLER #TIME 2D #PROGESS 30%

---
+++ CongNghePhanMem/Controllers/LoaiSachController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace CongNghePhanMem.Controllers
+{
+    public class LoaiSachController : Controller
+    {
+        // GET: LoaiSach
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoaiSachPartial()
+        {
+            var lst = cn.LoaiSaches.Take(18).ToList();
+            return PartialView(lst);
+        }
+       
+    }
+}
