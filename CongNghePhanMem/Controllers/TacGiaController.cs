8423cf60c28d5887da7030139f1133759c42357f
nguyenvantu <nguyenvantu439@gmail.com>
2017-11-22 00:15:29 +0700

    @CCPM-12 #COMMENT TAOtacgiacontroller #TIME 2D #PROGESS 100%

---
+++ CongNghePhanMem/Controllers/TacGiaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace CongNghePhanMem.Controllers
+{
+    public class TacGiaController : Controller
+    {
+        //GET: TacGia
+        public ActionResult Index()
+        {
+            return View();
+        }
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        public PartialViewResult TenTacGiaPartial()
+        {
+            var lst = cn.TacGias.ToList();
+            return PartialView(lst);
+        }
+        
+    }
+}
