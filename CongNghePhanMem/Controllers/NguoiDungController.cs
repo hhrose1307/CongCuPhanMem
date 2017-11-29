4329801935f37a9fc972c1ad422d6294b33ba7ef
nguyenvantu <nguyenvantu439@gmail.com>
2017-11-30 00:35:52 +0700

    @CCPM-15 #COMMENT TAoNGUOIDUNGCONTROLLER #TIME 2D #PROGESS 100%

--- CongNghePhanMem/Controllers/NguoiDungController.cs
+++ CongNghePhanMem/Controllers/NguoiDungController.cs
@@ -33,25 +33,7 @@ namespace CongNghePhanMem.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult DangNhap(FormCollection f, string Command)
-        {
-            if (Command == "Đăng nhập")
-            {
-                string sTen = f["txtTen"].ToString();
-                string sPass = f["txtPass"].ToString();
-                NguoiDung nd = cn.NguoiDungs.SingleOrDefault(n => n.TenDangNhap == sTen && n.MatKhau == sPass);
-                if (nd!= null)
-                {
-                    Session["TenDangNhap"] = nd;
-                    ViewBag.NguoiDung = nd.MaND;
-                    SetAlert("Đăng nhập thành công!", "success");
-                    return View();
-                }
-                SetAlert("Sai tài khoản, đăng nhập thất bại!", "warning");
-                return View();
-            }
-            return null;
-        }
+      
        
     
 }
