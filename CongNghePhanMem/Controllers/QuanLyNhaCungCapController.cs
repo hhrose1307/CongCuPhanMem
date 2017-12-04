fb2e65326f38f7256efb8bd8bc4afb3b9087c818
Tan Trieu <nttrieu1006@gmail.com>
2017-12-04 20:43:07 +0700

    @CCPM-7 #comment Tao hop dong QLNCCController #time 2h #done

--- CongNghePhanMem/Controllers/QuanLyNhaCungCapController.cs
+++ CongNghePhanMem/Controllers/QuanLyNhaCungCapController.cs
@@ -117,6 +117,70 @@ namespace CongNghePhanMem.Controllers
             }
             return RedirectToAction("NhaCungCap", "QuanLyNhaCungCap");
         }
-        
+        public ActionResult HopDong(int?page)
+        {
+            int pageSize = 20;
+            int pageNumber = (page ?? 1);
+            var hd = cn.HopDongNCCs.ToList().OrderBy(n=>n.MaHD).ToPagedList(pageNumber,pageSize);
+            return View(hd);
+        }
+        [HttpGet]
+        public ActionResult ThemHD()
+        {
+            ViewBag.MaNCC = new SelectList(cn.NhaCungCaps.ToList(), "MaNCC", "TenNCC");
+            return View();           
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ThemHD(HopDongNCC nd)
+        {
+            if(ModelState.IsValid)
+            {
+                HopDongNCC hd = new HopDongNCC();
+                hd.TenHD = nd.TenHD;
+                hd.MaNCC = nd.MaNCC;
+                hd.NgayKi = nd.NgayKi;
+                hd.NgayGiao = nd.NgayGiao;
+                hd.TinhTrangGiaoHang = nd.TinhTrangGiaoHang;
+                hd.TongTien = nd.TongTien;
+                hd.DaThanhToan = nd.DaThanhToan;
+                cn.HopDongNCCs.Add(hd);
+                cn.SaveChanges();
+                SetAlert("Thêm thành công!", "success");
+            }
+            return RedirectToAction("HopDong", "QuanLyNhaCungCap");
+        }
+        //xóa hợp đồng
+        public ActionResult XoaHD(int MaHD=0)
+        {
+            if(ModelState.IsValid)
+            {
+                ChiTietHopDongMua ct1 = cn.ChiTietHopDongMuas.FirstOrDefault(n => n.MaHD == MaHD);
+
+                if(ct1!=null)
+                {
+                    List<ChiTietHopDongMua> ls = cn.ChiTietHopDongMuas.Where(n => n.MaHD == MaHD).ToList();
+                    for (int i = 0; i <ls.Count; i++)
+                    {
+                        ChiTietHopDongMua ct = cn.ChiTietHopDongMuas.FirstOrDefault(n => n.MaHD == MaHD);
+                        cn.ChiTietHopDongMuas.Remove(ct);
+                        cn.SaveChanges();
+                    }
+                    HopDongNCC hd1 = cn.HopDongNCCs.SingleOrDefault(n => n.MaHD == MaHD);
+                    cn.HopDongNCCs.Remove(hd1);
+                    cn.SaveChanges();
+                    SetAlert("Xóa thành công", "success");
+                } 
+                else
+                {
+                    HopDongNCC hd = cn.HopDongNCCs.SingleOrDefault(n => n.MaHD == MaHD);
+                    cn.HopDongNCCs.Remove(hd);
+                    cn.SaveChanges();
+                    SetAlert("Xóa thành công", "success");
+                }
+                
+            }
+            return RedirectToAction("HopDong", "QuanLyNhaCungCap");
+        }
     }
 }
