93b1208e47b2cb6f7babb707c8e7e28acb3d0ff3
trieuvy <trieuvy61094@gmail.com>
2017-12-04 14:57:02 +0700

    @CCPM-16 #COMMENT taogiohang #TIME 1 NGAY #PROGRESS 10%

---
+++ CongNghePhanMem/Controllers/GioHangController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using System.Configuration;
+
+namespace CongNghePhanMem.Controllers
+{
+    public class GioHangController : Controller
+    {
+        //Lấy giỏ hàng
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+
+        protected void SetAlert(string message, string type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == "error")
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+        }
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+
+            }
+            return lstGioHang;
+        }
+        
+        
+    }
+}
