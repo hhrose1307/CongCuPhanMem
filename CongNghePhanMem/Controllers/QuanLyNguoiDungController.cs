911b00a8252272f8a28cece46069b93e3ac2595e
hoàn <linhanhxinh10@gmail.com>
2017-12-08 19:10:39 +0700

    @CCPM-5 #Edit nguoi dung #time 1d #progress 40%

--- CongNghePhanMem/Controllers/QuanLyNguoiDungController.cs
+++ CongNghePhanMem/Controllers/QuanLyNguoiDungController.cs
@@ -40,5 +40,39 @@ namespace CongNghePhanMem.Controllers
             return View(nguoidung);
         }
         [HttpGet]
+		public ActionResult Them()
+        {
+            return View();
+        }
+        [HttpPost]
+        //[ValidateInput(false)]
+        public ActionResult Them(TinhTrangNguoiDung nd)
+        {
+            if(ModelState.IsValid)
+            {
+                cn.TinhTrangNguoiDungs.Add(nd);
+                cn.SaveChanges();
+                SetAlert("Thêm thành công!", "success");
+            }
+            return RedirectToAction("TinhTrang", "QuanLyNguoiDung");
+        } 
+        [HttpGet]
+        public ActionResult SuaTT(int MaTT=0)
+        {
+            TinhTrangNguoiDung nd = cn.TinhTrangNguoiDungs.SingleOrDefault(n => n.MaTT == MaTT);
+            return View(nd);
+        }
+        [HttpPost]
+        public ActionResult SuaTT(TinhTrangNguoiDung nd)
+        {
+            if(ModelState.IsValid)
+            {
+                TinhTrangNguoiDung nd1 = cn.TinhTrangNguoiDungs.SingleOrDefault(n => n.MaTT == nd.MaTT);
+                nd1.TenTT = nd.TenTT;
+                cn.SaveChanges();
+                SetAlert("Sửa thành công!","success");
+            }
+            return RedirectToAction("TinhTrang", "QuanLyNguoiDung");         
+        }
     }
 }
