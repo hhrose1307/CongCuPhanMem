a7f3e10255d05f7a49a15363f0821d9faf7edb36
trieuvy <trieuvy61094@gmail.com>
2017-12-10 02:05:40 +0700

    @CCPM-18 #COMMENT giaodien #TIME 2 NGAY #PROGRESS 10%

---
+++ CongNghePhanMem/Controllers/GiaoDienController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace CongNghePhanMem.Controllers
+{
+    public class GiaoDienController : Controller
+    {
+        // GET: GiaoDien
+        public ActionResult Index()
+        {
+            return View();
+        }
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        public PartialViewResult Logo()
+        {
+            GiaoDien gd = cn.GiaoDiens.SingleOrDefault(n => n.ID == 1);
+            return PartialView(gd);
+        }
+        public PartialViewResult TieuDe()
+        {
+            GiaoDien tieude = cn.GiaoDiens.SingleOrDefault(n => n.ID == 2);
+            return PartialView(tieude);
+        }
+        public PartialViewResult DienThoai()
+        {   
+            GiaoDien dt = cn.GiaoDiens.SingleOrDefault(n => n.ID == 3);
+            return PartialView(dt);
+        }
+        public PartialViewResult DiaChi()
+        {
+            GiaoDien diachi = cn.GiaoDiens.SingleOrDefault(n => n.ID == 4);
+            return PartialView(diachi);
+        }
+        public PartialViewResult FaceBook()
+        {
+            GiaoDien fb = cn.GiaoDiens.SingleOrDefault(n => n.ID == 5);
+            return PartialView(fb);
+        }
+        public ActionResult Ykien(int ?page)
+        {
+            NguoiDung nd = (NguoiDung)Session["TenDangNhap"];
+            if (Session["TenDangNhap"] == null || Session["TenDangNhap"].ToString() == "")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                int pageSize = 2;
+                int pageNumber = (page ?? 1);
+                var lst = cn.PhanHois.Where(n => n.MaND == nd.MaND).ToList().OrderBy(n=>n.MaPH).ToPagedList(pageNumber,pageSize);
+                ViewBag.Ten = nd.HoTen;
+                ViewBag.Anh = nd.AnhDaiDien;
+                return View(lst);
+            }
+        }
+
+        
+    }
+}
