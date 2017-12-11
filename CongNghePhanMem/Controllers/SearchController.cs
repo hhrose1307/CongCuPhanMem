40a41c58f8c9b40c8595262502728e75a33f6c50
juncoi95 <jt070296@gmail.com>
2017-12-11 11:46:09 +0700

    @CCPM-19 #comment tao HttpPost trong SearchController #time 4d #progress 30%

--- CongNghePhanMem/Controllers/SearchController.cs
+++ CongNghePhanMem/Controllers/SearchController.cs
@@ -10,6 +10,30 @@ namespace CongNghePhanMem.Controllers
 {
     public class SearchController : Controller
     {
-       
+       // GET: Search
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        [HttpPost]
+        public ActionResult KetQuaSearch(FormCollection f, int? page,string Command)
+        {
+            if (Command == "Tìm kiếm")
+            {
+                string sTuKhoa = f["txtSearch"].ToString();
+                ViewBag.TuKhoa = sTuKhoa;
+                List<Sach> lstSearch = cn.Saches.Where(n => n.TenSach.Contains(sTuKhoa)).ToList();
+                //phân trang
+                int pageNumber = (page ?? 1);
+                int pageSize = 18;
+                //kieetm tra
+                if (lstSearch.Count == 0)
+                {
+                    ViewBag.ThongBao = "Không tìm thấy sản phẩm nào";
+                    return View(cn.Saches.OrderBy(n => n.TenSach).ToPagedList(pageNumber, pageSize));
+                }
+                ViewBag.ThongBao = "Đã tìm thấy" + lstSearch.Count + "kết quả.";
+                return View(lstSearch.OrderBy(n => n.TenSach).ToPagedList(pageNumber, pageSize));
+            }
+            else
+                return View();
+        }
     }
 }
